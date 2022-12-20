85e15246de1b1677c1e4bd11400b94dc10c93fc6
Keshav057-max <keshav.057@gmail.com>
2022-12-20 15:41:13 +0530

    jenkins demo

---
+++ src/test/java/com/jenkins/framework/jdemo.java
@@ -0,0 +1,14 @@
+package com.jenkins.framework;
+
+import org.testng.annotations.Test;
+
+public class jdemo {
+	@Test
+	public static void A1() {
+		
+		System.out.println("welcome to jenkins");
+		System.out.println("welcome to mvn");
+		
+	}
+
+}
